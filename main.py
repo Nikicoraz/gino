81faea1820be2ebc101781fb71d4681b6c8ab049
Nicola Corato <nicolacorato05@gmail.com>
2021-01-29 13:22:31 +0100

    Aggiunto comando bot

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
+#!venv/Scripts/python.exe
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+bot = commands.Bot(command_prefix='$')
 client = discord.Client()
 TOKEN = os.getenv('TOKEN')
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    for guild in client.guilds:
+    print('We have logged in as {0.user}'.format(bot))
+    for guild in bot.guilds:
         print(f"Bot is being used in {guild.name} (id:{guild.id})")
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
     msg = message.content.lower()
     if msg.__contains__('hello there'):
         await message.channel.send('General Kenobi!')
-    elif msg.startswith('n/'):
-        if msg.__contains__('gigi'):
-            await message.author.create_dm()
-            await message.author.dm_channel.send("GIGI IL MIO ACERRIMO NEMICO")
+
+
+
+
+bot.run(TOKEN)
 client.run(TOKEN)
