bc6f5a54316d3af750c01743650ef351738bec2b
Nicola Corato <nicolacorato05@gmail.com>
2021-02-02 12:25:53 +0100

    Bicchiere di vino

--- main.py
+++ main.py
@@ -69,7 +69,8 @@ def switch_messaggi(msg):
         'gigi': 'IL MIO ACERRIMO NEMICO',
         'nigga': 'Un po\' razzista ma ok',
         'negro': 'Un po\' razzista ma ok',
-        'pepsiman': ['Pepsi Man!🍶', 'https://lh3.googleusercontent.com/proxy/FSpQFPlPO8ac96WB44FOI0aLlEV_7tudsOpTAcRBEtodKVbwqo8fO1I_Zy5ztQ43mUtAKBjuezACQFLihjkZWjUVvkJvZXri7PhSJBH_yMtOEhjlToNTH7UCbts']
+        'pepsiman': ['Pepsi Man!🍶', 'https://lh3.googleusercontent.com/proxy/FSpQFPlPO8ac96WB44FOI0aLlEV_7tudsOpTAcRBEtodKVbwqo8fO1I_Zy5ztQ43mUtAKBjuezACQFLihjkZWjUVvkJvZXri7PhSJBH_yMtOEhjlToNTH7UCbts'],
+        '🍷':'🍷'
         }
 
     for key in dic.keys():
