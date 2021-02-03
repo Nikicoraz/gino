fe5cdcd77451377b6cb90157c49f7a3505cdbf62
Nicola Corato <nicolacorato05@gmail.com>
2021-02-03 21:46:21 +0100

    sistemati link pepsi man

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def switch_messaggi(msg):
         'gigi': 'IL MIO ACERRIMO NEMICO',
         'nigga': 'Un po\' razzista ma ok',
         'negro': 'Un po\' razzista ma ok',
-        'pepsiman': ['Pepsi Man!🍶', 'https://lh3.googleusercontent.com/proxy/FSpQFPlPO8ac96WB44FOI0aLlEV_7tudsOpTAcRBEtodKVbwqo8fO1I_Zy5ztQ43mUtAKBjuezACQFLihjkZWjUVvkJvZXri7PhSJBH_yMtOEhjlToNTH7UCbts', 'https://www.youtube.com/watch?v=q_9SsX7HJhE'],
+        'pepsiman': ['Pepsi Man!🍶', 'https://www.player.it/wp-content/uploads/2018/12/Pepsiman-il-videogioco.jpg', 'https://youtu.be/z54MpfR3XE4'],
         '🍷':'🍷',
         'grazie':'Prego',
         ':pepesad:':'F',
