eb8b489db7e1dcdfadaf265c992b93b4cdd3965e
Nicola Corato <nicolacorato05@gmail.com>
2021-03-17 10:07:50 +0100

    rimesso regex perche' senno' e' in mezzo

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ insulti = []
 
 url_pattern = r'(http|https)://.*'
 youtube_url = r'(http|https)://(www.youtube.com|youtu.be)/.*'
-emoji_pattern = r'^<.*:[a-zA-Z0-9_-]+:[0-9]+>$'
+animated_emoji_pattern = r'^<a:[a-zA-Z0-9_-]+:[0-9]+>$'
 
 def rigenera_insulti():
     global insulti
@@ -394,7 +394,7 @@ async def on_message(message):
             loc = {}
             exec(messaggio[1], globals(), loc)
             await message.channel.send(embed=loc['msgg'])
-        elif re.match(emoji_pattern, messaggio):
+        elif re.match(animated_emoji_pattern, messaggio):
             await message.delete()
             await message.channel.send(messaggio)
         else:
